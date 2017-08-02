8885f22acc45de266345cceb8127aa704d74c230
olnogues <olnogues@gmail.com>
2017-08-02 21:52:53 +0100

    link

--- report_index_set.py
+++ report_index_set.py
@@ -8,6 +8,10 @@ import os
 def call(aresObj):
   """
   """
+  path = os.path.join("user_reports", aresObj.http['POST']['report'])
+  if not os.path.exists(path):
+      os.makedirs(path)
+      return '%s - Report created %s' % aresObj.http['POST']['report']
 
-
-  return '%s - Report created' % aresObj.http['POST']['report']
+  else:
+    return '%s - Already exist' % aresObj.http['POST']['report']
