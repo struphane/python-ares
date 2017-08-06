f05ccfafc5d7f5d9ab695f5aaf8a80d931451999
olnogues <olnogues@gmail.com>
2017-08-07 00:30:51 +0100

    Create folder

--- ares/report_index_set.py
+++ ares/report_index_set.py
@@ -6,17 +6,16 @@
 import os
 
 def call(aresObj):
-  """
-  """
+  """ """
   path = os.path.join("user_reports", aresObj.http['POST']['report'])
   if not os.path.exists(path):
       os.makedirs(path)
       reportFile = open(r"%s\%s.py" % (path, aresObj.http['POST']['report']), "w")
       reportFile.write("''' [SCRIPT COMMENT] '''\n\n")
-      reportFile.write("AJAX_CALL = {'testAjax': 'MyRepotTestAjax.py'} # Ajax call definition\n")
+      reportFile.write("AJAX_CALL = ['MyRepotTestAjax.py'] # Ajax call definition\n")
       reportFile.write("CHILD_PAGES = {'test': 'MyRepotTestChild.py',} # Child pages call definition\n\n")
       reportFile.write("def report(aresObj, localPath=None):\n\n\n\n")
-      reportFile.write("  return aresObj.html(localPath)")
+      reportFile.write("  return aresObj")
       reportFile.close()
       return '%s - Report created %s' % aresObj.http['POST']['report']
 
