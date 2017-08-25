3d41a9a9091cb95c953120bf65de7edf7bd366e0
pynog <30602860+pynog@users.noreply.github.com>
2017-08-25 17:21:55 +0100

    Javascript

--- static/js/ares.js
+++ static/js/ares.js
@@ -1,14 +1,21 @@
+/*
+ 
+ */
 
 function getDict(dict, key, dflt){
+    // Return a default value in a dictionary
     if (key in dict) return dict[key];
     else return dflt
 }
 function getDataFromRecordSet(data, cols) {
+    // Return the list of tuples as expected in a graph
+    // It will use a temporary dictionary for the aggregation
     var tmpDict = {};
     for (var i = 0, len = data.length; i < len; i++) {
         tmpDict[data[i][cols[0]]] = getDict(tmpDict, data[i][cols[0]], 0)  + parseFloat(data[i][cols[1]]) ;
     }
 
+    // Build the list of tuple that is expected by the graph
     var filterData = [];
     for (var key in tmpDict) {
         filterData.push([key, tmpDict[key]]);
