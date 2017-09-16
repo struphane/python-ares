15a3956870605e497bcbbf086f0cc92b08abd4b3
pynog <30602860+pynog@users.noreply.github.com>
2017-09-16 23:46:06 +0100

    Dedicated module for the Buttons

---
+++ ares/Lib/AresHtmlButton.py
@@ -0,0 +1,209 @@
+"""
+
+"""
+
+import os
+import json
+
+from click import echo
+from ares.Lib import AresHtml
+from ares.Lib import AresItem
+from flask import render_template_string
+from datetime import datetime
+
+
+class Button(AresHtml.Html):
+  """
+  Python wrapper to the HTML Button component
+
+  Input value should be a String
+
+  Default class parameters
+    - CSS Default Class = button
+  """
+  alias, cssCls = 'button', 'btn btn-success'
+  reference = 'https://www.w3schools.com/tags/tag_button.asp'
+
+  def __str__(self):
+    """ Return the String representation of HTML button """
+    return '<button %s type="button" style="margin-bottom:10px;">%s</button>' % (self.strAttr(), self.vals)
+
+  @classmethod
+  def aresExample(cls, aresObj):
+    return aresObj.button("MyButton")
+
+  def post(self, evenType, url, **kwargs):
+    """
+      Post method to get data directly by interacting with the page
+      https://api.jquery.com/jquery.post/
+    """
+    # Part dedicated to run before the Ajax call
+
+    jsData = []
+    for key, data in kwargs.items():
+      if key not in ('cssCls', ):
+        if issubclass(data.__class__, AresHtml.Html):
+          jsData.append("%s: %s" % (key, data.val))
+
+    url = render_template_string('''{{ url_for(\'%s\') }}''' % url)
+    preAjax = AresItem.Item("var %s = %s.html();" % (self.htmlId, self.jqId))
+    preAjax.add(0, "%s.html('<i class=\"fa fa-spinner fa-spin\"></i> Processing'); " % self.jqId)
+    preAjax.add(0, kwargs.get('preAjaxJs', ''))
+
+    super(Button, self).post(evenType, url, "{%s}" % ",".join(jsData), "%s ; %s.html('%s') ;" % (kwargs.get('js', ''), self.jqId, self.vals),
+    str(preAjax), kwargs.get('redirectUrl', ''))
+
+
+class ButtonRemove(AresHtml.Html):
+  """
+  Python wrapper to the HTML Button component
+
+  Input value should be a String
+
+  Default class parameters
+    - CSS Default Class = btn btn-danger
+    - glyphicon = remove
+  """
+  glyphicon, cssCls = 'remove', 'btn btn-danger'
+  reference =  'http://www.kodingmadesimple.com/2015/04/custom-twitter-bootstrap-buttons-icons-images.html'
+  alias = 'remove'
+
+  def __str__(self):
+    """ Return the String representation of a HTML Style Twitter button """
+    return '<button type="button" %s><span class="fa fa-%s">&nbsp;%s</span></button>' % (self.strAttr(), self.glyphicon, self.vals)
+
+  @classmethod
+  def aresExample(cls, aresObj):
+    return aresObj.remove()
+
+
+class ButtonDownload(ButtonRemove):
+  """
+  Python wrapper to the HTML Button component
+
+  Input value should be a String
+
+  Default class parameters
+    - CSS Default Class = btn btn-success
+    - glyphicon = download
+  """
+  glyphicon, cssCls = 'download', 'btn btn-success'
+  reference =  'http://www.kodingmadesimple.com/2015/04/custom-twitter-bootstrap-buttons-icons-images.html'
+  alias = 'download'
+
+  @classmethod
+  def aresExample(cls, aresObj):
+    return aresObj.download()
+
+
+class ButtonDownloadAll(ButtonRemove):
+  """
+  Python wrapper to the HTML Button component
+
+  Input value should be a String
+
+  Default class parameters
+    - CSS Default Class = btn btn-success
+    - glyphicon = downloadAll
+  """
+  glyphicon, cssCls = 'cloud-download', 'btn btn-success'
+  reference =  'http://www.kodingmadesimple.com/2015/04/custom-twitter-bootstrap-buttons-icons-images.html'
+  alias = 'downloadAll'
+
+  @classmethod
+  def aresExample(cls, aresObj):
+    return aresObj.downloadAll()
+
+
+class ButtonOk(ButtonRemove):
+  """
+  Python wrapper to the HTML Button component
+
+  Input value should be a String
+
+  Default class parameters
+    - CSS Default Class = btn btn-success
+    - glyphicon = ok
+  """
+  glyphicon, cssCls = 'check-square-o', 'success'
+  reference =  'http://www.kodingmadesimple.com/2015/04/custom-twitter-bootstrap-buttons-icons-images.html'
+  alias = 'ok'
+
+  @classmethod
+  def aresExample(cls, aresObj):
+    return aresObj.ok("OK Button")
+
+
+class ButtonRefresh(ButtonRemove):
+  """
+  Python wrapper to the HTML Button Refresh component
+
+  """
+  glyphicon, cssCls = 'refresh', 'btn btn-success'
+  reference =  'http://www.kodingmadesimple.com/2015/04/custom-twitter-bootstrap-buttons-icons-images.html'
+  alias = 'refresh'
+
+  def __init__(self, aresObj, vals, recordSet, ajaxScript, cssCls=None):
+    """
+    Instantiate the object and attached the recordSet to the event
+    This object will also store the data to a text file.
+    """
+    super(ButtonRefresh, self).__init__(aresObj, vals, cssCls)
+    self.ajaxScript = ajaxScript
+    self.dataFileName = "%s.json" % id(recordSet)
+    dataPath = os.path.join(aresObj.http['DIRECTORY'], 'data')
+    if not os.path.exists(dataPath):
+      os.makedirs(dataPath)
+    recordSetJson = open(os.path.join(dataPath, self.dataFileName), "w")
+    json.dump(recordSet, recordSetJson)
+    recordSetJson.close()
+
+  def click(self, jsFnc):
+    """ Add the corresponding event when the button is clicked """
+    self.js('click',
+            render_template_string('''
+                // The first part will update the file
+                $.post("{{ url_for('ares.ajaxCall', report_name='%s', script='%s', file_name='%s') }}", function(data) {
+                    data = JSON.parse(data) ;
+                    // Then it will update the reports
+                    %s
+                });
+              ''' % (self.aresObj.http['REPORT_NAME'], self.ajaxScript, self.dataFileName, jsFnc)
+            ))
+
+  def __str__(self):
+    """ Return the String representation of a HTML Style Twitter button """
+    return '<button type="button" %s><span class="fa fa-%s">&nbsp;%s</span></button>' % (self.strAttr(), self.glyphicon, self.vals)
+
+
+class GeneratePdf(ButtonRemove):
+  alias = "generatePdf"
+  glyphicon, cssCls = "book", "btn btn-default"
+  source = r"http://pdfmake.org/#/gettingstarted"
+
+  def __init__(self, aresObj, fileName=None, cssCls=None): # Hack: I need the whole aresObj as param since I need to retrieve everything that has been created so far
+    super(GeneratePdf, self).__init__(aresObj, "", cssCls)
+
+    if fileName is None:
+      fileName = "%s_%s" % (aresObj.reportName.strip().replace(" ", "_") if hasattr(aresObj, "reportName") else "ares_export", datetime.now())
+
+    content = []
+    styles = {"Title": {"fontSize": 22, "bold": "true", "italic": "true", "alignment": "left"}}
+    for objId in aresObj.content:
+      htmlObj = aresObj.htmlItems[objId]
+      content.append({"text": htmlObj.vals, "style": htmlObj.__class__.__name__})
+    varName = "%s_Content" % self.htmlId
+    varTxt = "var %s = %s;" % (varName, json.dumps({"content": content, "style": styles}))
+
+    """
+    var generatepdf_2_Content = {content: [{text: 'Mrflex Monitoring', style: 'Title'}],
+                             styles: {Title: {fontSize: 40, bold: true, italic: true}}};
+
+      $("#generatepdf_2").on("click", function(event)
+      {
+          pdfMake.createPdf(generatepdf_2_Content).download('Mrflex_Monitoring_2017-08-24 23:28:20.011011.pdf');
+      }
+      );
+    """
+    self.jsEvent["var"] = varTxt#"var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };"
+    self.jsEvent["click"] = AresJs.JQueryEvents(self.htmlId, '$("#%s")' % self.htmlId, "click", "pdfMake.createPdf(%s).download('%s.pdf');" % (varName, fileName))
