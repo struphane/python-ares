e887eb0b9a6f9ada407bd9b1bce394b5a120f929
olnogues <olnogues@gmail.com>
2017-09-24 18:07:16 +0100

    basic table

--- ares/reports/_AresEx/AresExTables.py
+++ ares/reports/_AresEx/AresExTables.py
@@ -14,6 +14,35 @@ def report(aresObj):
   """
   # Produce the recordSet
   recordSet = ExAjaxRec.getRecordSet()
+  aresObj.title("Basic Table with Multiple headers")
+  aresObj.paragraph('''
+    The header is defined as a list of dictionary.
+    The dictionary will use two important keys:
+        - key: the key which should be present in your recordSet of data
+               For example if you are expecting to get the below data the key can be PTF
+                  recordSet = [{'PTF': 1, 'VAL': 10}, {'PTF': 2, 'VAL': 2}]
+        - colName: the column header in the above example you can define header = [{'key': 'PTF', 'colName': 'Portfolio'}]
+
+    In the table display below the header is defined as below:
+    '''
+  )
+  aresObj.preformat(aresObj.code('''
+    header = [ {'key': 'PTF', 'colName': 'Portfolio', 'colspan': 1, 'rowspan': 2},
+               {'key': 'PTF2', 'colName': 'Portfolio 2', 'colspan': 1},
+               {'key': 'VAL', 'colName': 'Value', 'colspan': 2}],
+
+               [{'key': 'PTF3', 'colName': 'Portfolio 3'},
+                {'key': 'PTF2', 'colName': 'Portfolio 2', 'colspan': 1},
+                {'key': 'VAL1', 'colName': 'Value 1', 'colspan': 1}],
+
+              [{'key': 'PTF', 'colName': 'Portfolio'},
+                  {'key': 'CCY', 'colName': 'Currency'},
+                  {'key': 'VAL2', 'colName': 'Value 2'},
+                  {'key': 'VAL3', 'colName': 'Value 3'}]
+    '''
+  ))
+
+  aresObj.title3("Result")
   table = aresObj.table(recordSet, [
                                     [{'key': 'PTF', 'colName': 'Portfolio', 'colspan': 1, 'rowspan': 2},
                                      {'key': 'PTF2', 'colName': 'Portfolio 2', 'colspan': 1},
