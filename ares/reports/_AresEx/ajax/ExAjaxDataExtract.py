15f2aed3e212b6e06e7c60d054ea7e4cde2d555e
olnogues <olnogues@gmail.com>
2017-09-26 22:40:29 +0100

    Typo issue

--- ares/reports/_AresEx/ajax/ExAjaxDataExtract.py
+++ ares/reports/_AresEx/ajax/ExAjaxDataExtract.py
@@ -11,5 +11,5 @@ def call(aresObj):
   """
 
   aresObj.wData('BasicExtract', "%s_%s_1.txt" % (aresObj.http['COB'], aresObj.http['NODE']))
-  file = aresObj.rDara('BasicExtract', "%s_%s_2.txt" % (aresObj.http['COB'], aresObj.http['NODE']))
+  file = aresObj.rData('BasicExtract', "%s_%s_2.txt" % (aresObj.http['COB'], aresObj.http['NODE']))
   return {"status": "Updated", "data": aresObj.listDataFrom("BasicExtract"), "content": ""}
