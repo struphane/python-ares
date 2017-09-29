21cd1dad180e1f059ed1fff4641cbe388192e434
pynog <30602860+pynog@users.noreply.github.com>
2017-09-29 23:03:17 +0100

    Add Js and Css Module mapping

---
+++ ares/Lib/AresJsModules.py
@@ -0,0 +1,16 @@
+__author__ = 'HOME'
+
+
+
+jsImport = {
+  'ares': 'ares.js',
+  'bootstrap': 'bootstrap.js',
+  'jquery': 'jquery-3.2.1.min.js',
+
+}
+
+cssImport = {
+  'jquery-ui': 'jquery-ui.css',
+
+
+}
