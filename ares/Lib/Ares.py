c1799fccedcd59ee384e7123d7403b9959671b5a
olnogues <olnogues@gmail.com>
2017-09-30 16:18:44 +0100

    Ares Js and CSS Import

--- ares/Lib/Ares.py
+++ ares/Lib/Ares.py
@@ -457,5 +457,8 @@ class Report(object):
           jsSection.append("\n".join(jsFncs))
 
     print self.jsImports, self.cssImport
+    importMng = AresJsModules.ImportManager()
+    importMng.cssResolve(self.cssImport)
+    importMng.jsResolve(self.jsImports)
     jsSection.append("nv.addGraph(function() {\n %s \n});" % "\n".join(jsGraphs))
     return "\n".join(onloadParts), "\n".join(htmlParts), "\n".join(jsSection)
--- ares/Lib/AresJsModules.py
+++ ares/Lib/AresJsModules.py
@@ -2,10 +2,12 @@
 
 """
 
-jsImport = {
-  'ares': 'ares.js',
+from flask import render_template_string
 
-  'bootstrap': 'bootstrap.min.js',
+JS_IMPORTS = {
+  'ares': {'modules': ['ares.js']},
+
+  'bootstrap': {'modules': ['bootstrap.min.js']},
 
   # Javascript packages to handle DataTables
   'dataTables': {'req': ['jquery'],
@@ -13,10 +15,10 @@ jsImport = {
                              'dataTables.responsive.min.js', 'buttons.colVis.min.js']},
 
   # Jquery package
-  'jquery': 'jquery-3.2.1.min.js',
+  'jquery': {'modules': ['jquery-3.2.1.min.js']},
 
   # Javascript packages for the PDF transformation
-  'pdfmake': ['pdfmake.min.js', 'vfs_fonts.js'],
+  'pdfmake': {'modules': ['pdfmake.min.js', 'vfs_fonts.js']},
 
   # Javascript dependencies for D3 and NVD2 components
   'd3': {'req': ['jquery'],
@@ -31,16 +33,16 @@ jsImport = {
 
   }
 
-cssImport = {
+CSS_IMPORTS = {
   'jquery-ui': {'modules': ['jquery-ui.css'],
-                'url': '',
                 },
 
-  'dataTables': {'modules': ['jquery.dataTables.min.css', 'responsive.dataTables.min.css', 'buttons.dataTables.min.css']},
+  'dataTables': {'modules': ['jquery.dataTables.min.css', 'responsive.dataTables.min.css', 'buttons.dataTables.min.css'],
+                 },
 
   'bootstrap': {'modules': ['bootstrap.min.css', 'metro-bootstrap.min.css']},
 
-  'font-awesome': {'modules': 'font-awesome.min.css'},
+  'font-awesome': {'modules': ['font-awesome.min.css']},
 
   'd3': {'modules': ['svg.css', 'nv.d3.css']},
 
@@ -51,4 +53,32 @@ class ImportManager(object):
   """
 
   """
+
+  def cssResolve(self, cssAliases):
+    """ Return the list of CSS modules to add to the header """
+    cssList = []
+    for cssAlias in cssAliases:
+      cssDefinition = CSS_IMPORTS[cssAlias]
+      for css in cssDefinition['modules']:
+        if 'url' in cssDefinition :
+          # In this case we want to use a specific module not necessarily in the server
+          # Flask will not be used to translate the URL automatically
+          cssList.append('<link rel="stylesheet" href="%s/%s" type="text/css">' % (cssDefinition['url'], css))
+        else:
+          cssList.append('<link rel="stylesheet" href="{{ url_for(\'static\',filename=\'css/%s\') }}" type="text/css">' % css)
+    return render_template_string("\n".join(cssList))
+
+  def jsResolve(self, jsAliases):
+    """ Return the list of Javascript modules to add to the header """
+    jsList = []
+    for jsAlias in jsAliases:
+      jsDefinition = JS_IMPORTS[jsAlias]
+      for js in jsDefinition['modules']:
+        if 'url' in jsDefinition :
+          # In this case we want to use a specific module not necessarily in the server
+          # Flask will not be used to translate the URL automatically
+          jsList.append('<script language="javascript" type="text/javascript" src="%s/%s"></script>' % (jsDefinition['url'], js))
+        else:
+          jsList.append('<script language="javascript" type="text/javascript" src="{{ url_for(\'static\',filename=\'js/%s\') }}"></script>' % js)
+    return render_template_string("\n".join(jsList))
 
