0d4a9c44e43bb5fffa3eecc3d1971bb9a4190a8d
pynog <30602860+pynog@users.noreply.github.com>
2017-10-03 00:29:05 +0100

    Add files via upload

--- Lib/flask_dummy.py
+++ Lib/flask_dummy.py
@@ -1,7 +1,11 @@
-""" Flask local wrapper
-
-"""
+from __future__ import print_function
+import AresInstall
+import re
 
 def render_template_string(val):
   """ Wrap the function to convert the URL """
+  res = re.search('<link rel="stylesheet" href="{{ url_for\(\'static\',filename=\'css/([a-zA-Z0-9.]*)\'\) }}" type="text/css">', val)
+  if res is not None:
+	  return "%s/static/css/%s" % (AresInstall.SERVER_PATH, res.group(1).replace("\\", "/"))
+
   return val
