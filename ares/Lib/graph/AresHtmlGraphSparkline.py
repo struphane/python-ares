b217dce8c4cac39851b24ab50b2ac429b979f12a
olnogues <olnogues@gmail.com>
2017-10-23 23:34:19 +0100

    chart

--- ares/Lib/graph/AresHtmlGraphSparkline.py
+++ ares/Lib/graph/AresHtmlGraphSparkline.py
@@ -3,9 +3,12 @@
 
 """
 
-from ares.Lib.html import AresHtmlContainer
+import json
+from Libs import AresChartsService
+from ares.Lib.html import AresHtmlGraphSvg
 
-class NvD3SparkLinePlus(AresHtmlContainer.Svg):
+
+class NvD3SparkLinePlus(AresHtmlGraphSvg.MultiSvg):
   """
 
   """
@@ -13,7 +16,8 @@ class NvD3SparkLinePlus(AresHtmlContainer.Svg):
   references = ['http://nvd3.org/examples/line.html']
   __chartStyle = {
     'margin': '{left:70}',
-    'x': 'function(d,i) { return i }',
+    'x': "function(d) { return d[0]; }",
+    'y': "function(d) { return d[1]; }",
     'showLastValue': 'true',
     'showLastValue': "function(d) {return d3.time.format('%x')(new Date(data[d].x))}",
   }
@@ -21,18 +25,26 @@ class NvD3SparkLinePlus(AresHtmlContainer.Svg):
   reqCss = ['bootstrap', 'font-awesome', 'd3']
   reqJs = ['jquery', 'd3']
 
-  def graph(self):
-    """ Add the Graph definition in the Javascript method """
-    self.aresObj.jsGraphs.append(
-      '''
-        var %s = nv.models.%s()
-            .%s ;
+  def filterSerie(self, key):
+    """  """
+    self.filterAKey = key
 
-        %s
+  def processData(self):
+    """ produce the different recordSet with the level of clicks defined in teh vals and set functions """
+    recordSet = AresChartsService.toMultiSeries(self.vals, self.chartKeys, self.selectedX , self.chartVals)
+    for key, recordSets in recordSet.items():
+      for recrordSet in recordSets:
+        if recrordSet['key'] ==  self.filterAKey:
+          self.aresObj.jsGlobal.add("%s_%s = %s ;" % (self.htmlId, key, json.dumps(recrordSet['values'])))
 
-        d3.select("#%s svg").datum(%s).call(%s);
-
-        nv.utils.windowResize(%s.update);
-      ''' % (self.htmlId, self.chartObject, self.attrToStr(), self.propToStr(),
-             self.htmlId, self.dataFnc(), self.htmlId, self.htmlId)
-    )
+  def jsUpdate(self):
+    dispatchChart = []
+    for displathKey, jsFnc in self.dispatch.items():
+      dispatchChart.append("%s.pie.dispatch.on('%s', function(e) { %s ;})" % (self.htmlId, displathKey, jsFnc))
+    return '''
+              var %s = nv.models.%s().%s ;
+              %s
+              d3.select("#%s svg").datum(%s).call(%s);
+              nv.utils.windowResize(%s.update);
+          ''' % (self.htmlId, self.chartObject, self.attrToStr(), self.propToStr(),
+                 self.htmlId, self.jqData, self.htmlId, self.htmlId)
