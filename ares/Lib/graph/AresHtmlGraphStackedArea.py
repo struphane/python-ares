057aa938824435d78df5b520becb556ed3d526f1
olnogues <olnogues@gmail.com>
2017-10-23 22:14:27 +0100

    StackedArea Charts

--- ares/Lib/graph/AresHtmlGraphStackedArea.py
+++ ares/Lib/graph/AresHtmlGraphStackedArea.py
@@ -36,7 +36,6 @@ class NvD3StackedArea(AresHtmlGraphSvg.MultiSvg):
   def processData(self):
     """ produce the different recordSet with the level of clicks defined in teh vals and set functions """
     recordSet = AresChartsService.toMultiSeries(self.vals, self.chartKeys, self.selectedX , self.chartVals)
-    print(recordSet)
     for key, vals in recordSet.items():
       self.aresObj.jsGlobal.add("%s_%s = %s ;" % (self.htmlId, key, json.dumps(vals)))
 
