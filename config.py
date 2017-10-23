19b943e21899a0415dac3e8a434b52716dfd73de
Tinels <nelson.allain@gmail.com>
2017-10-23 22:51:07 +0100

    new endpoint for deployment

--- config.py
+++ config.py
@@ -5,6 +5,7 @@
 import os
 
 ARES_FOLDER = 'ares'
+ARES_MAIN_DB_LOCATION = os.path.join('system', 'sqlite', 'main.db')
 ARES_USERS_LOCATION = 'user_reports'
 ARES_USERS_DELETED_FOLDERS = 'user_deleted_reports'
 ARES_SQLITE_FILES_LOCATION = 'static/sql_config'
