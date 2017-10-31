2d1e7f6d831326bb01a29743fa7e22a1dc97361e
olnogues <olnogues@gmail.com>
2017-10-31 19:30:22 +0000

    File Parser

---
+++ Libs/AresFileParser.py
@@ -0,0 +1,51 @@
+""" Service dedicated to parse files according to a standard Configurations
+@author: Olivier Nogues
+
+Those configuration are mandatories in the framework as they will allow you to display
+in a comprehensive manner the data in the framework. Those configurations are important
+for outputs but also static files.
+
+You can retrieve data from this module by interating on it.
+Also there is a function in Ares to allow you to iterate on files which are configured in your
+environment.
+
+Please do not use this module directly in your production code.
+
+"""
+
+import csv
+
+class FileParser(object):
+  """
+  """
+  hdrLines = 0
+
+  def __init__(self, inFile):
+    """ Create a file object """
+    self.__inputFile = inFile
+    for _ in range(self.hdrLines):
+      self.__inputFile.next()
+    self.header, self.colCnvFnc, keyMapCol = [], {}, {}
+    for i, col in enumerate(self.cols):
+      self.header.append(col['colName'])
+      keyMapCol[col['colName']] = i
+      if 'convertFnc' in col:
+        self.colCnvFnc[i] = col['convertFnc']
+    vCols = getattr(self, 'vCols', [])
+    self.vHeader, self.vColCnvFnc = {}, {}
+    for i, vCol in enumerate(vCols):
+      self.vHeader[i] = vCol['colName']
+      map(lambda x: keyMapCol[x], vCol['mapCols'])
+      self.vColCnvFnc[i] = (vCol['mapCols'], vCol['convertFnc'])
+
+  def __iter__(self):
+    """ Iterator to return a line """
+    for line in self.__inputFile:
+      reader  = csv.reader([line], skipinitialspace=True, delimiter=self.delimiter)
+      for row in reader:
+        rec = dict(zip(self.header, row))
+        for i, transFnc in self.colCnvFnc.items():
+          rec[self.header[i]] = transFnc(row[i])
+        for i, name in self.vHeader.items():
+          rec[name] = self.vColCnvFnc[i][1](*[rec[col] for col in self.vColCnvFnc[i][0]])
+        yield rec
