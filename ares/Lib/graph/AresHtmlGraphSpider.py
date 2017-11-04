b160db0be9f71e450cd04dcaeebebb81aeb837d0
pynog <30602860+pynog@users.noreply.github.com>
2017-11-04 17:43:45 +0000

    Add multipler

--- ares/Lib/graph/AresHtmlGraphSpider.py
+++ ares/Lib/graph/AresHtmlGraphSpider.py
@@ -65,10 +65,10 @@ class D3SpiderChart(AresHtmlGraphSvg.MultiSvg):
   def __str__(self):
     """ Return the svg container """
     self.processData()
-    categories = AresHtmlRadio.Radio(self.aresObj, [key for key, _ in self.chartKeys], cssAttr={'display': 'None'} if len(self.chartKeys) == 1 else {})
+    categories = AresHtmlRadio.Radio(self.aresObj, [key for key, _, _ in self.chartKeys], cssAttr={'display': 'None'} if len(self.chartKeys) == 1 else {})
     categories.select(self.selectedChartKey)
     self.dynKeySelection = categories.val # The javascript representation of the radio
-    values = AresHtmlRadio.Radio(self.aresObj, [val for val, _ in self.chartVals], cssAttr={'display': 'None'} if len(self.chartVals) == 1 else {})
+    values = AresHtmlRadio.Radio(self.aresObj, [val for val, _, _ in self.chartVals], cssAttr={'display': 'None'} if len(self.chartVals) == 1 else {})
     values.select(self.selectedChartVal)
     self.dynValSelection = values.val # The javascript representation of the radio
     categories.click([self])
