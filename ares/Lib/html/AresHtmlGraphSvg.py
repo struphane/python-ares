44eb0ce390ecdd6b50a79240068c60304aa422eb
pynog <30602860+pynog@users.noreply.github.com>
2017-11-04 18:27:02 +0000

    Integer format

--- ares/Lib/html/AresHtmlGraphSvg.py
+++ ares/Lib/html/AresHtmlGraphSvg.py
@@ -170,9 +170,9 @@ class Svg(AresHtml.Html):
   def yAxisAsInt(self, withCcy=None):
     """ Return the data as integer """
     if withCcy is None:
-      self.chartProps("yAxis", {"tickFormat": "function(d){ return d3.format(',')(d) }" })
+      self.addChartProp("yAxis", {"tickFormat": "function(d){ return d3.format(d) }" })
     else:
-      self.chartProps("yAxis", {"tickFormat": "function(d){ return d3.format(',')(d) + ' %s' }" % withCcy})
+      self.addChartProp("yAxis", {"tickFormat": "function(d){ return d3.format(d) + ' %s' }" % withCcy})
 
 
   def processDataMock(self, cat=None, val=None):
