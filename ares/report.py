d416cc2f919041ab31930933a6e14c42abfb84bd
pynog <30602860+pynog@users.noreply.github.com>
2017-11-14 01:56:04 +0000

    Show params

--- ares/report.py
+++ ares/report.py
@@ -287,15 +287,18 @@ def run_report(report_name, script_name, user_id):
       del sys.modules[script_name]
 
     mod = __import__(script_name) # run the report
-    fnct = 'report'
-    for param in getattr(mod, 'HTTP_PARAMS', []):
-      if not param['code'] in reportObj.http:
-        if not 'dflt' in param:
-          fnct = 'params'
-          break
-          
-        else:
-          reportObj.http[param['code']] = param['dflt']
+    if reportObj.http.get('show_params') == '1':
+      fnct = 'params'
+    else:
+      fnct = 'report'
+      for param in getattr(mod, 'HTTP_PARAMS', []):
+        if not param['code'] in reportObj.http:
+          if not 'dflt' in param:
+            fnct = 'params'
+            break
+
+          else:
+            reportObj.http[param['code']] = param['dflt']
     # Set some environments variables which can be used in the report
     reportObj.http.update( {'FILE': script_name, 'REPORT_NAME': report_name, 'DIRECTORY': userDirectory} )
     for fileConfig in getattr(mod, 'FILE_CONFIGS', []):
