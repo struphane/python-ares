3b649933b6228c30838169ecdecc41e28c4e99bf
Olivier <olnogues@gmail.com>
2017-11-20 21:21:27 +0000

    Set order in charts

--- ares/Lib/html/AresHtmlGraphSvg.py
+++ ares/Lib/html/AresHtmlGraphSvg.py
@@ -128,6 +128,11 @@ class Svg(AresHtml.Html):
     self.chartKeys = [self.header[key] for key in keys]
     self.selectedChartKey = keys[selected] if selected is not None else keys[0]
 
+  def setKeyOrder(self, xValsList):
+    """ For the order on the abscisse, this can only work with 1 series """
+    self.addChartProp('xAxis', {'tickValues': [i for i in range(len(xValsList))]})
+    self.addChartProp('xAxis', {'tickFormat': "function(d){ return %s[d] }" % json.dumps(xValsList)})
+
   def setVals(self, vals, selected=None):
     """ Set a default value for the graph """
     self.chartVals = [self.header[val] for val in vals]
@@ -209,6 +214,11 @@ class MultiSvg(Svg):
   def setX(self, x):
     """ """
     self.selectedX = self.header[x]
+
+  def setXOrder(self, xValsList):
+    """ For the order on the abscisse """
+    self.addChartProp('xAxis', {'tickValues': [i for i in range(len(xValsList))]})
+    self.addChartProp('xAxis', {'tickFormat': "function(d){ return %s[d] }" % json.dumps(xValsList)})
 
   def setExtVals(self, keys, components):
     """ Link the result to the different components on the page """
