9d83e9b8aecae4898cd736f29f4c1a85f4c7cb40
Tinels <nelson.allain@gmail.com>
2017-11-29 08:14:49 +0000

    file permissioning only on data types

--- static/sql_config/create_file.sql
+++ static/sql_config/create_file.sql
@@ -2,4 +2,4 @@ REPLACE INTO file_map (alias, file_type, disk_name) VALUES ('%(fileCode)s', '%(f
 INSERT INTO file_auth (file_id, team_id, temp_owner)
 SELECT file_map.file_id, team_def.team_id, '%(username)s'
 FROM file_map, team_def
-WHERE file_map.disk_name = '%(filename)s' and team_def.team_name = '%(team_name)s';
+WHERE file_map.disk_name = '%(filename)s' AND team_def.team_name = '%(team_name)s' AND file_map.file_type = 'data';
