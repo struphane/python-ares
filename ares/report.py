30d693debbb104d7c22d834ae4ae9a2edc404618
Tinels <nelson.allain@gmail.com>
2017-12-03 19:14:25 +0000

    mark words for editor

--- ares/report.py
+++ ares/report.py
@@ -358,7 +358,7 @@ def run_report(report_name, script_name, user_id):
     pyKeywords = ['False', 'class', 'finally', 'is', 'return', 'None', 'continue',
                   'for','lambda','try','True','def','from','nonlocal','while','and',
                   'del','global','not','with','as','elif','if','or','yield','assert',
-                  'else','import','pass','break','except','in','raise']
+                  'else','import','pass','break','except','in','raise', 'self']
     scriptData = []
     for line in inFile:
       tempData = line
