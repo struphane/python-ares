da06b0d193d3d9c21ac62f7accd282934cb3f89c
pynog <30602860+pynog@users.noreply.github.com>
2017-12-11 18:33:48 +0000

    Externalise the colours

--- static/js/venn.js
+++ static/js/venn.js
@@ -1222,7 +1222,10 @@
 
     /*global console:true*/
 
-    function VennDiagram() {
+    function VennDiagram(colourChart) {
+        var colourScheme = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+        if (typeof colourChart !== 'undefined') { colourScheme = colourChart;}
+
         var width = 600,
             height = 350,
             padding = 15,
@@ -1241,7 +1244,7 @@
             // so this is the same as d3.schemeCategory10, which is only defined in d3 4.0
             // since we can support older versions of d3 as long as we don't force this,
             // I'm hackily redefining below. TODO: remove this and change to d3.schemeCategory10
-            colourScheme = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"],
+
             colourIndex = 0,
             colours = function(key) {
                 if (key in colourMap) {
