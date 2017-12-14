232e4e39d1c2919bd0fa277b332fa213b0cbc431
pynog <30602860+pynog@users.noreply.github.com>
2017-12-14 13:39:47 +0000

    Create new entry for Donut

--- ares/Lib/graph/AresHtmlGraphDonut.py
+++ ares/Lib/graph/AresHtmlGraphDonut.py
@@ -60,3 +60,50 @@ class NvD3Donut(AresHtmlGraphSvg.Svg):
   def alertVal(self):
     """ Add a click even on the chart  """
     self.dispatch['elementClick'] = "alert('selected value = ' + e.data) ;"
+
+
+class XNvD3Donut(AresHtmlGraphSvg.XSvg):
+  """ NVD3 Pie Chart python interface """
+  alias, chartObject = 'donut', 'pieChart'
+  references = ['http://nvd3.org/examples/pie.html']
+
+  __chartStyle = {'showLabels': 'true',
+                  'labelThreshold': .05,
+                  'labelType': '"percent"',
+                  'donut': 'true',
+                  'donutRatio': 0.35,
+                  'color': 'd3.scale.ordinal().range(%s).range()' % json.dumps(AresHtmlGraphSvg.Svg.colorCharts),
+                  'x': "function(d) { return d.key; }",
+                  'y': "function(d) { return d.value; }"}
+
+  __svgProp = {
+    'transition': '',
+  }
+
+  # Required modules
+  reqCss = ['bootstrap', 'font-awesome', 'd3']
+  reqJs = ['d3']
+  clickJsFnc = None
+
+  def jsUpdate(self, data=None):
+    """ Javascript function to build and update the chart based on js variables stored as globals to your report  """
+    # Dispatch method to add events on the chart (in progress)
+    data = data if data is not None else self.jqData
+    if self.clickJsFnc is None:
+      self.clickJsFnc = 'alert(e.data.key);'
+
+    #self.click()
+    dispatchChart = ["%s.pie.dispatch.on('%s', function(e) { %s ;})" % (self.htmlId, displathKey, jsFnc) for displathKey, jsFnc in self.dispatch.items()]
+    return '''
+              %(chartDimension)s ;
+              d3.select("#%(htmlId)s svg").remove(); d3.select("#%(htmlId)s").append("svg");
+              var %(htmlId)s = nv.models.%(chartObject)s().%(chartAttr)s ; %(chartProp)s
+              d3.select("#%(htmlId)s svg").style("height", '%(height)spx').datum(%(data)s)%(svgProp)s.call(%(htmlId)s); %(dispatchChart)s ;
+              nv.utils.windowResize(%(htmlId)s.update);
+            ''' % {'htmlId': self.htmlId, 'chartObject': self.chartObject, 'chartAttr': self.attrToStr(), 'chartDimension': data['vars'],
+                   'chartProp': self.propToStr(), 'height': self.height, 'data': data['data'], 'svgProp': self.getSvg(),
+                   'dispatchChart': ";".join(dispatchChart)}
+
+  def click(self):
+    """ Add a click even on the chart  """
+    self.dispatch['elementClick'] = self.clickJsFnc
