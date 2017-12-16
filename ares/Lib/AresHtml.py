4405bd3e0064dfa23a82482d12dc2176ea8c9e6e
pynog <30602860+pynog@users.noreply.github.com>
2017-12-16 19:02:51 +0000

    Age on cells

--- ares/Lib/AresHtml.py
+++ ares/Lib/AresHtml.py
@@ -85,10 +85,10 @@ class Html(object):
   hidden = False
 
   # Different nuances to diaplay the age of a cell, After 15 days there is no nuance anymore
-  ageColors = {
-    'red': ['#ffe5e5', '#ffcccc', '#ffb2b2', '#ff9999', '#ff7f7f', '#ff6666', '#ff4c4c', '#ff3232', '#ff1919', '#ff0000', '#e50000', '#cc0000', '#b20000', '#990000', '#7f0000'],
-    'blue': ['#e5e5ff', '#ccccff', '#b2b2ff', '#9999ff', '#7f7fff', '#6666ff', '#4c4cff', '#3232ff', '#1919ff', '#0000ff', '#0000e5', '#0000cc', '#0000b2', '#000099', '#00007f'],
-    'green': ['#e5ffe5', '#ccffcc', '#b2ffb2', '#99ff99', '#7fff7f', '#66ff66', '#4cff4c', '#32ff32', '#19ff19', '#00ff00', '#00e500', '#00cc00', '#00b200', '#009900', '#007f00'],
+  __ageColors = {
+    'red': ['#ffffff', '#ffe5e5', '#ffcccc', '#ffb2b2', '#ff9999', '#ff7f7f', '#ff6666', '#ff4c4c', '#ff3232', '#ff1919', '#ff0000', '#e50000', '#cc0000', '#b20000', '#990000', '#7f0000'],
+    'blue': ['#ffffff', '#e5e5ff', '#ccccff', '#b2b2ff', '#9999ff', '#7f7fff', '#6666ff', '#4c4cff', '#3232ff', '#1919ff', '#0000ff', '#0000e5', '#0000cc', '#0000b2', '#000099', '#00007f'],
+    'green': ['#ffffff', '#e5ffe5', '#ccffcc', '#b2ffb2', '#99ff99', '#7fff7f', '#66ff66', '#4cff4c', '#32ff32', '#19ff19', '#00ff00', '#00e500', '#00cc00', '#00b200', '#009900', '#007f00'],
   }
 
   def __init__(self, aresObj, vals, cssCls=None, cssAttr=None):
@@ -140,6 +140,8 @@ class Html(object):
     """ Property to get the jquery value of the HTML objec in a python HTML object """
     return '%s.val()' % self.jqId
 
+  def getAgeColor(self):
+    return self.__ageColors['red']
 
   # CSS Classes management
   def addClass(self, cssCls):
