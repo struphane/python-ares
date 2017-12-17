1f248ee45d3949cad8bb98be743954418679a507
pynog <30602860+pynog@users.noreply.github.com>
2017-12-17 15:05:31 +0000

    Fix alerts

--- ares/Lib/html/AresHtmlAlert.py
+++ ares/Lib/html/AresHtmlAlert.py
@@ -1,28 +1,24 @@
-#
-# class Notification(object):
-#   """ """
-#   def __init__(self, aresObj):
-#     """ """
-#     self.aresObj = aresObj
-#
+""" Python Wrapper to display alerts in reports
+@author: Olivier Nogues
+
+"""
+
 
 from ares.Lib import AresHtml
-from ares.Lib import AresItem
 
 
 class DangerAlert(AresHtml.Html):
   """
 
   """
-  level = 'Danger'
+  level, cssCls = 'Danger', ['alert-danger']
   backgroundColor = '#f44336'
   closeButton = False
-  cssCls = ['alert-danger']
-  jsEvent = None
+  reqJs, reqCss = ['bootstrap'], ['bootstrap']
 
   def __init__(self, aresObj, title, value, countNotif, closeButton=False, backgroundColor=None, cssCls=None, cssAttr=None):
     """ Instantiate the Danger notification box """
-    super(DangerAlert, self).__init__(aresObj, '', self.cssCls, cssAttr)
+    super(DangerAlert, self).__init__(aresObj, value, self.cssCls, cssAttr)
     self.title = title
     self.closeButton = closeButton
     self.countNotif = countNotif
@@ -30,14 +26,14 @@ class DangerAlert(AresHtml.Html):
       self.backgroundColor = backgroundColor
 
   def __str__(self):
-    """ """
+    """ Return the string representation for an alert """
     if self.closeButton:
-      items = AresItem.Item('<div class="alert %s %s notif fade in" style="top:%spx">' % (self.cssCls, 'alert-dismissable', self.countNotif * 70))
-      items.add(1, '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>')
+      item = ['<div class="alert %s %s notif in" style="top:%spx; z-index:300;right:5px;width:400px">' % (" ".join(self.attr['class']), 'alert-dismissable', 50 + self.countNotif * 70)]
+      item.append('<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>')
     else:
-      items = AresItem.Item('<div class="alert notif %s" style="top:%spx">' % (self.cssCls, self.countNotif * 10))
-    items.add(0, '<strong>%s!</strong> %s </div>' % (self.title, self.vals))
-    return str(items)
+      item = ['<div class="alert notif %s" style="top:%spx; z-index:300;right:5px;width:300px">' % (" ".join(self.attr['class']), 50 + self.countNotif * 10)]
+    item.append('<strong>%s!</strong> %s </div>' % (self.title, self.vals))
+    return "".join(item)
 
 
 class SuccessAlert(DangerAlert):
@@ -45,7 +41,7 @@ class SuccessAlert(DangerAlert):
   level = 'Success'
   backgroundColor = '#4CAF50'
   closeButton = True
-  cssCls = 'alert-success'
+  cssCls = ['alert-success']
 
 
 class WarningAlert(DangerAlert):
@@ -53,7 +49,7 @@ class WarningAlert(DangerAlert):
   level = 'Info'
   backgroundColor = '#2196F3'
   closeButton = True
-  cssCls = 'alert-warning'
+  cssCls = ['alert-warning']
 
 
 class InfoAlert(DangerAlert):
@@ -61,5 +57,5 @@ class InfoAlert(DangerAlert):
   level = 'Warning'
   backgroundColor = '#ff9800'
   closeButton = True
-  cssCls = 'alert-info'
+  cssCls = ['alert-info']
 
